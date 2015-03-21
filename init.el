0acaea81a7c060142b4ce980c07a6a305ac41e70
Pavel Repin <prepin@gmail.com>
2015-03-21 01:43:01 -0700

    Super basic boilerplate.
    Based on: http://milkbox.net/note/single-file-master-emacs-configuration/

---
+++ init.el
@@ -0,0 +1,50 @@
+;;; init.el --- My emacs configuration file
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen
+(setq inhibit-startup-screen t)
+
+;;;; package.el
+(require 'package)
+(setq package-user-dir "~/.emacs.d/elpa/")
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(defun mp-install-rad-packages ()
+  "Install only the sweetest of packages."
+  (interactive)
+  (package-refresh-contents)
+  (mapc #'(lambda (package)
+            (unless (package-installed-p package)
+              (package-install package)))
+        '(ido-ubiquitous
+	  magit
+	  paredit
+	  projectile)))
+
+;;;; macros
+(defmacro after (mode &rest body)
+  "`eval-after-load' MODE evaluate BODY."
+  (declare (indent defun))
+  `(eval-after-load ,mode
+     '(progn ,@body)))
+
+;;;; global key bindings
+
+
+;;;; emacs lisp
+(defun imenu-elisp-sections ()
+  (setq imenu-prev-index-position-function nil)
+  (add-to-list 'imenu-generic-expression '("Sections" "^;;;; \\(.+\\)$" 1) t))
+
+(add-hook 'emacs-lisp-mode-hook 'imenu-elisp-sections)
+
+
+;;; init.el ends here
