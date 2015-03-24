d01f57e4e0bf5b7d2371dc655bf9a7cf38c166f6
Pavel Repin <prepin@gmail.com>
2015-03-23 20:15:38 -0700

    From Prelude: death to tabs and more.

--- init.el
+++ init.el
@@ -27,6 +27,58 @@
   `(eval-after-load ,mode
      '(progn ,@body)))
 
+
+;;;; editor settings from Prelude
+
+;; Death to the tabs!  However, tabs historically indent to the next
+;; 8-character offset; specifying anything else will cause *mass*
+;; confusion, as it will change the appearance of every existing file.
+;; In some cases (python), even worse -- it will change the semantics
+;; (meaning) of the program.
+;;
+;; Emacs modes typically provide a standard means to change the
+;; indentation width -- eg. c-basic-offset: use that to adjust your
+;; personal indentation width, while maintaining the style (and
+;; meaning) of any files you load.
+(setq-default indent-tabs-mode nil)   ;; don't use tabs to indent
+(setq-default tab-width 8)            ;; but maintain correct appearance
+
+;; Newline at end of file
+(setq require-final-newline t)
+
+;; delete the selection with a keypress
+(delete-selection-mode t)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; autosave the undo-tree history
+(setq undo-tree-history-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq undo-tree-auto-save-history t)
+
+;; revert buffers automatically when underlying files are changed externally
+(global-auto-revert-mode t)
+
+;; hippie expand is dabbrev expand on steroids
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev
+                                         try-expand-dabbrev-all-buffers
+                                         try-expand-dabbrev-from-kill
+                                         try-complete-file-name-partially
+                                         try-complete-file-name
+                                         try-expand-all-abbrevs
+                                         try-expand-list
+                                         try-expand-line
+                                         try-complete-lisp-symbol-partially
+                                         try-complete-lisp-symbol))
+
+;; smart tab behavior - indent or complete
+(setq tab-always-indent 'complete)
+
+
 ;;;; global key bindings
 
 ;; replace buffer-menu with ibuffer
@@ -55,13 +107,13 @@
 (use-package paredit
   :ensure t
   :config (progn
-	    ;; Enable `paredit-mode' in the minibuffer, during `eval-expression'.
-	    (defun conditionally-enable-paredit-mode ()
-	      (if (eq this-command 'eval-expression)
-		  (paredit-mode 1)))
-	    (add-hook 'minibuffer-setup-hook 'conditionally-enable-paredit-mode)
-	    (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
-	    (add-hook 'clojure-mode-hook 'paredit-mode)))
+            ;; Enable `paredit-mode' in the minibuffer, during `eval-expression'.
+            (defun conditionally-enable-paredit-mode ()
+              (if (eq this-command 'eval-expression)
+                  (paredit-mode 1)))
+            (add-hook 'minibuffer-setup-hook 'conditionally-enable-paredit-mode)
+            (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+            (add-hook 'clojure-mode-hook 'paredit-mode)))
 
 
 ;;;; rainbow-delimiters
@@ -85,16 +137,16 @@
 (use-package company
   :ensure t
   :config (progn
-	    ;; Use company-mode in all buffers
-	    (add-hook 'after-init-hook 'global-company-mode)))
+            ;; Use company-mode in all buffers
+            (add-hook 'after-init-hook 'global-company-mode)))
 
 
 ;;;; ido-ubiquitous
 (use-package ido-ubiquitous
   :ensure t
   :init (progn
-	  (ido-mode +1)
-	  (ido-ubiquitous-mode +1)))
+          (ido-mode +1)
+          (ido-ubiquitous-mode +1)))
 
 
 ;;;; miscellaneous customizations
@@ -107,9 +159,6 @@
 ;; This tells various "git" commands not to pipe their output through
 ;; "less" or similar.
 (setenv "PAGER" "cat")
-
-;; Turn on the auto-revert mode, globally.
-(global-auto-revert-mode t)
 
 ;; At work I sometimes use a frame that's 166 characters wide.  When I
 ;; do that, I find that many windows split horizontally -- for
